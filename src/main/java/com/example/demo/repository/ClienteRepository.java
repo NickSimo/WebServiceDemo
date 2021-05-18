276fbe5603926b3620b38cb8b59a4de6ac76ea1c
NSim <nicolosimoncelli@gmail.com>
2021-05-18 21:05:00 +0200

    nuovo esempio

--- src/main/java/com/example/demo/repository/ClienteRepository.java
+++ src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Cliente;
 import com.example.demo.entity.rowmapper.ClienteRowMapper;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,13 +11,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Data
+@RequiredArgsConstructor
 public class ClienteRepository {
+
     private final JdbcTemplate jdbcTemplate;
 
-    public ClienteRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
 
 
