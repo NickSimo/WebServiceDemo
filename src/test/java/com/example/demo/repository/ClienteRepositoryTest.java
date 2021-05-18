98c62da330d430c9119ae54c47a10ffef06c42f8
NSim <nicolosimoncelli@gmail.com>
2021-05-18 21:31:37 +0200

    nuovo esempio

--- src/test/java/com/example/demo/repository/ClienteRepositoryTest.java
+++ src/test/java/com/example/demo/repository/ClienteRepositoryTest.java
@@ -2,16 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.FakeDatabaseConfiguration;
 import com.example.demo.entity.Cliente;
-import com.example.demo.repository.ClienteRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @Import(FakeDatabaseConfiguration.class)
