4d0e7aa4fa5e0209334d19443e5a0d47fb3460ec
NSim <nicolosimoncelli@gmail.com>
2021-05-20 16:05:57 +0200

    a

--- src/main/java/com/example/demo/controller/ClientiController.java
+++ src/main/java/com/example/demo/controller/ClientiController.java
@@ -5,7 +5,6 @@ import com.example.demo.service.EstraiClientiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
