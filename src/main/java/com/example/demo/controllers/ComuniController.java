efb3385f8891b7bf74659a18a5500baa355240e0
Simone Pavanello <simone.pavanello@addvalue.it>
2021-10-15 10:39:28 +0200

    rimozione annotazione useless

--- src/main/java/com/example/demo/controllers/ComuniController.java
+++ src/main/java/com/example/demo/controllers/ComuniController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controllers;
 
-import com.example.demo.dto.Cliente;
 import com.example.demo.dto.Comuni;
 import com.example.demo.services.ComuniAdapterService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +17,6 @@ public class ComuniController {
   @Autowired ComuniAdapterService comuniAdapterService;
 
   @GetMapping(value = "/elenco")
-  @Transactional()
   public List<Comuni> comuni() throws Exception {
     return comuniAdapterService.chiama();
   }
